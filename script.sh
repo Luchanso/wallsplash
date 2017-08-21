8024036d34df1f6730b5035e556687fe1a369397
Luchansky Oleg <oleg.luchansky@iqoption.com>
2017-08-21 15:52:21 +0300

    Fix script

--- script.sh
+++ script.sh
@@ -2,4 +2,4 @@
 
 curl -v "https://unsplash.it/1920/1080/?random" > currentWallpaper.jpg
 wallpaper=`realpath ./currentWallpaper.jpg`
-gsettings set org.gnome.desktop.background picture-uri "file://$wallpaper"e
+gsettings set org.gnome.desktop.background picture-uri "file://$wallpaper"
